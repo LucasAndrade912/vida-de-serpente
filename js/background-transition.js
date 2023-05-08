e0dd27f8c781efbfb8fa24578878c81c66b4c1fd
Lucas Andrade <lucasantos.dev@gmail.com>
2023-05-08 20:02:22 -0300

    fix background loading bug

--- js/background-transition.js
+++ js/background-transition.js
@@ -1,4 +1,8 @@
-const backgrounds = ['./images/01.png', './images/02.png', './images/03.png'];
+import img1 from '../images/01.png';
+import img2 from '../images/02.png';
+import img3 from '../images/03.png';
+
+const backgrounds = [img1, img2, img3];
 let backgroundNumber = 0;
 
 export function changeBackgroundLoop() {
