5cf48b306427c114fa68eb598039c02a373ae3da
Lucas Andrade <lucasantos.dev@gmail.com>
2023-07-05 11:02:42 -0300

    Corrigindo estilo de botao

--- src/app/components/FormularioContato.jsx
+++ src/app/components/FormularioContato.jsx
@@ -1,37 +1,42 @@
 "use client"
+
+import '../css/formulario-contato.css'
 import { useState } from "react";
 
 export default function FormularioContato() {
-    const [email, setemail] = useState()
-    function enviarFormulario(){if (!validarEmail(email)) alert('Email inválido')}
+    const [email, setEmail] = useState()
+
+    function enviarFormulario() {
+        if (!validarEmail(email)) alert('Email inválido')
+    }
+
     function validarEmail(email) {
         const expressaoRegular = /^[^\s@]+@[^\s@]+\.[^\s@]+[^.]$/;
         const regexPonto = /\.\./;
         return expressaoRegular.test(email) && !regexPonto.test(email);
-      }
-    
-    
+    }
     
     function validarTelefone(telefone) {
         const regex = /^\(?\d{2}\)?[-.\s]?\d{4,5}[-.\s]?\d{4}$/;
         return regex.test(telefone);
-      }
+    }
     
     return (
-    <form onSubmit={enviarFormulario} className="formulario" method="post" action="#">
+        <form onSubmit={enviarFormulario} className="formulario" method="post" action="#">
             <input type="text" placeholder="Qual o seu nome? :)" required="" />
-            <input onChange={e => setemail(e.target.value)} type="email" placeholder="Seu e-mail" required="" />
+            <input onChange={e => setEmail(e.target.value)} type="email" placeholder="Seu e-mail" required="" />
+
             <textarea
-            name="mensagem"
-            type="text"
-            placeholder="Escreva aqui sua mensagem."
-            required=""
-            defaultValue={""}
-            />     
+                name="mensagem"
+                type="text"
+                placeholder="Escreva aqui sua mensagem."
+                required=""
+                defaultValue={""}
+            />
+
             <div className="btn">
                 <input type="submit" defaultValue="Enviar" className="btn-submit" />
-            </div>   
-    </form>
-    
+            </div>
+        </form>
     ); 
 };
--- src/app/contato/page.jsx
+++ src/app/contato/page.jsx
@@ -1,16 +1,15 @@
 import "../css/contato.css";
-import FormularioContato from "../components/formulario";
+import FormularioContato from "../components/FormularioContato";
 
 export default function Contato() {
     return (
     <div className="contato-content">
-    <div className="div-contato">
-      <p className="tit-contato">
-        Encontrou alguma serpente na sua região ou tem alguma dúvida?
-      </p>
-      <FormularioContato/>    
-      
-    </div>
+        <div className="div-contato">
+            <p className="tit-contato">
+                Encontrou alguma serpente na sua região ou tem alguma dúvida?
+            </p>
+            <FormularioContato/>
+        </div>
     </div>
   );
 };
